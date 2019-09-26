f6e43ece6c7a2cc8a9d846b32d322c70823f5c2c
thierryturpin <thierry@turpin.be>
2019-09-26 14:23:08 +0200

    Let's fail the tests

--- tests/test_csv_to_parquet2.py
+++ tests/test_csv_to_parquet2.py
@@ -8,7 +8,7 @@ import logging
 class MyTest(unittest.TestCase):
     def test(self):
 
-        mask_ratio = 2
+        mask_ratio = 3
 
         spark = SparkSession.builder.appName("csv_to_parquet_test").getOrCreate()
 
